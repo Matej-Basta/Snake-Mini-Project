986cd432a640da112f38a295334f128a95819e40
matej <matej.basta@email.cz>
2024-01-28 20:02:58 +0100

    icons added with links to github + linkedin

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,12 @@
+import { BsGithub, BsLinkedin } from "react-icons/bs";
+
 export default function Footer() {
     return (
-        <div className="footer"></div>
+        <div className="footer">
+            <div className="icons-container">
+                <a href="https://github.com/Matej-Basta" target="_blank"><BsGithub /></a>
+                <a href="https://www.linkedin.com/in/mat%C4%9Bj-ba%C5%A1ta-265325143/" target="_blank"><BsLinkedin /></a>
+            </div>
+        </div>
     )
 }
